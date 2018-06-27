ef68df3e6508150d8235da88d2aa0cf0736f4a01
igorrabelo <39538971+igorrabelo@users.noreply.github.com>
2018-06-27 20:48:23 -0300

    Update config.py
    Atualização de arquivo.

--- config.py
+++ config.py
@@ -13,3 +13,7 @@
 #MONGO_PASSWORD='chuvinha10'
 #MONGO_DBNAME='ubiqs'
 MONGO_URI='mongodb://igor:igor123@ds149960.mlab.com:49960/mobquestions'
+
+REDIS_HOST = 'redis-19805.c15.us-east-1-2.ec2.cloud.redislabs.com'
+REDIS_PORT = 19805
+REDIS_PASSWORD = 'TWizEVRLkIqSMslo4lqFtJaA9EpBWeLK'
